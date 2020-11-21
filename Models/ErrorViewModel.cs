b4513e3481934e19a94189896b636e8bece63ca9
gadevinusha <gadevinusha@gmail.com>
2020-11-21 22:08:09 +0530

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebAppAD.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
